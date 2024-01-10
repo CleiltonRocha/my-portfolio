de393f4b11b111ef01b020ac9afd3156e18af68f
Cleilton Rocha <cleiltonrv30@gmail.com>
2024-01-09 22:39:21 -0300

    fix: removing cursor component

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,41 +1,41 @@
-'use client'
+// 'use client'
 
-import { gsap } from 'gsap'
-import { useEffect } from 'react'
+// import { gsap } from 'gsap'
+// import { useEffect } from 'react'
 
-export function Cursor() {
-  useEffect(() => {
-    const links = document.querySelectorAll('a, button')
-    const cursorText = document.querySelector('.cursor-text') as HTMLElement
+// export function Cursor() {
+//   useEffect(() => {
+//     const links = document.querySelectorAll('a, button')
+//     const cursorText = document.querySelector('.cursor-text') as HTMLElement
 
-    const onMouseMove = (event: MouseEvent) => {
-      const { clientX, clientY } = event
-      gsap.to('.cursor', { x: clientX, y: clientY })
-    }
+//     const onMouseMove = (event: MouseEvent) => {
+//       const { clientX, clientY } = event
+//       gsap.to('.cursor', { x: clientX, y: clientY })
+//     }
 
-    const onMouseEnterLink = () => {
-      gsap.to('.cursor', { scale: 2 })
-    }
+//     const onMouseEnterLink = () => {
+//       gsap.to('.cursor', { scale: 2 })
+//     }
 
-    const onMouseLeaveLink = () => {
-      gsap.to('.cursor', { scale: 1 })
-      cursorText.style.display = 'none'
-    }
+//     const onMouseLeaveLink = () => {
+//       gsap.to('.cursor', { scale: 1 })
+//       cursorText.style.display = 'none'
+//     }
 
-    document.addEventListener('mousemove', onMouseMove)
+//     document.addEventListener('mousemove', onMouseMove)
 
-    links.forEach((link) => {
-      link.addEventListener('mouseenter', onMouseEnterLink)
-      link.addEventListener('mouseleave', onMouseLeaveLink)
-    })
-  }, [])
+//     links.forEach((link) => {
+//       link.addEventListener('mouseenter', onMouseEnterLink)
+//       link.addEventListener('mouseleave', onMouseLeaveLink)
+//     })
+//   }, [])
 
-  return (
-    <div
-      id="cursor"
-      className="custom-cursor cursor mobile:hidden desktop:block"
-    >
-      <span className="cursor-text">View</span>
-    </div>
-  )
-}
+//   return (
+//     <div
+//       id="cursor"
+//       className="custom-cursor cursor mobile:hidden desktop:block"
+//     >
+//       <span className="cursor-text">View</span>
+//     </div>
+//   )
+// }
